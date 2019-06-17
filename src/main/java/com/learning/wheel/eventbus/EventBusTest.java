46f960fa2be8c188bc534c09b403ab879cd3c3dc
hangwei <hangwei@shinemo.com>
2019-06-17 17:48:53 +0800

    c

--- src/main/java/com/learning/wheel/eventbus/EventBusTest.java
+++ src/main/java/com/learning/wheel/eventbus/EventBusTest.java
@@ -3,6 +3,7 @@ package com.learning.wheel.eventbus;
 import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,6 +13,9 @@ import java.util.concurrent.Executors;
  * @create: 2019/06/13 17:59
  */
 final public class EventBusTest {
+    private static EventBus eventBus = new AsyncEventBus(Executors.newFixedThreadPool(2));
+
+
     private static class EventA {
         public String toString() {
             return "A 类型事件";
@@ -54,6 +58,7 @@ final public class EventBusTest {
             Thread.sleep(1000);
             System.out.println(name + "订阅的是C事件,接收到:" + e);
             System.out.format("%s sleep 完成%n", name);
+            eventBus.post(new EventD());
         }
         @Subscribe
         public void onEvent(EventD e) throws InterruptedException {
@@ -62,6 +67,7 @@ final public class EventBusTest {
             Thread.sleep(1000);
             System.out.println(name + "订阅的是D事件,接收到:" + e);
             System.out.format("%s sleep 完成%n", name);
+            eventBus.post(new EventC());
         }
         @Subscribe
         public void onEvent(DeadEvent de) {
@@ -69,28 +75,37 @@ final public class EventBusTest {
         }
     }
 
+    private static class EventListenerA {
 
-    public static void main(String[] args) throws InterruptedException {
+        public void onEvent(EventA e) {
+            System.out.println("111111111");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         EventBus eb = new EventBus();
-        eb.register(new EventListener());
-        System.out.println("----------发送事件A---------");
-        eb.post(new EventA());
-        System.out.println("----------发送事件B---------");
-        eb.post(new EventB());
-        System.out.println("----------发送事件X---------");
-        eb.post(new EventX());
-        System.out.println("----------发送事件C---------");
-        ExecutorService threadPool = Executors.newCachedThreadPool();
-        eb = new AsyncEventBus(threadPool);
-        eb.register(new EventListener());
-        for (int i = 0; i < 20; i++) {
-            eb.post(new EventC());
-        }
-        System.out.println("----------发送事件D---------");
-        for (int i = 0; i < 20; i++) {
-            eb.post(new EventD());
-        }
-        Thread.sleep(2000);
-        threadPool.shutdown();
+//        eb.register(new EventListener());
+//        System.out.println("----------发送事件A---------");
+//        eb.post(new EventA());
+//        System.out.println("----------发送事件B---------");
+//        eb.post(new EventB());
+//        System.out.println("----------发送事件X---------");
+//        eb.post(new EventX());
+//        System.out.println("----------发送事件C---------");
+//        ExecutorService threadPool = Executors.newCachedThreadPool();
+//        eb = new AsyncEventBus(threadPool);
+//        eventBus.register(new EventListenerA());
+////        for (int i = 0; i < 20; i++) {
+////            eb.post(new EventC());
+////        }
+////        System.out.println("----------发送事件D---------");
+//
+//        eventBus.post(new EventA());
+
+        Method method = EventListenerA.class.getMethod("onEvent", EventA.class);
+        method.invoke(new EventListener(),new EventA());
+
+//        Thread.sleep(2000);
+//        threadPool.shutdown();
     }
 }
