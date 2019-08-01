512f0a8fc8a8014cf7ff7ff809fd8ce7564d9bbe
hangwei <hangwei@shinemo.com>
2019-08-01 11:08:36 +0800

    c

--- src/main/java/com/learning/wheel/completableFuture/CompletableTest.java
+++ src/main/java/com/learning/wheel/completableFuture/CompletableTest.java
@@ -3,8 +3,7 @@ package com.learning.wheel.completableFuture;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
+import java.util.function.*;
 
 /**
  * @program wheel
@@ -74,5 +73,75 @@ public class CompletableTest {
         });
 
         TimeUnit.SECONDS.sleep(2);
+    }
+    //一个线程依赖另外一个线程的结果，串行化
+    private static void thenApply() throws Exception{
+        CompletableFuture<Long> future = CompletableFuture.supplyAsync(new Supplier<Long>() {
+            @Override
+            public Long get() {
+                long result = new Random().nextInt(100);
+                System.out.println("result1="+result);
+                return result;
+            }
+        }).thenApply(new Function<Long, Long>() {
+            @Override
+            public Long apply(Long t) {
+                long result = t*5;
+                System.out.println("result2="+result);
+                return result;
+            }
+        });
+
+        long result = future.get();
+        System.out.println(result);
+    }
+    //  执行结束对结果的处理(异步)，又返回结果
+    public static void handle() throws Exception{
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(new Supplier<Integer>() {
+
+            @Override
+            public Integer get() {
+                int i= 10/0;
+                return new Random().nextInt(10);
+            }
+        }).handle(new BiFunction<Integer, Throwable, Integer>() {
+            @Override
+            public Integer apply(Integer param, Throwable throwable) {
+                int result = -1;
+                if(throwable==null){
+                    result = param * 2;
+                }else{
+                    System.out.println(throwable.getMessage());
+                }
+                return result;
+            }
+        }).handleAsync(new BiFunction<Integer, Throwable, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Throwable throwable) {
+                int result = 2;
+                if(throwable==null){
+                    result = integer * 3;
+                }else{
+                    System.out.println(throwable.getMessage());
+                }
+                return result;
+            }
+        });
+        System.out.println(future.get());
+    }
+
+    private static void thenAccept() throws Exception{
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return new Random().nextInt();
+            }
+        }).thenAccept(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+        future.get();
     }
 }
