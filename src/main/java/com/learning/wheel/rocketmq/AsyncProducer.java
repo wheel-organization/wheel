d3c2ba17d4474c52fddb6321ce4528a5a93aacd5
hangwei <hangwei@shinemo.com>
2020-06-30 11:17:43 +0800

    c

---
+++ src/main/java/com/learning/wheel/rocketmq/AsyncProducer.java
@@ -0,0 +1,47 @@
+package com.learning.wheel.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @program wheel
+ * @author: hangwei
+ * @create: 2019/09/02 17:44
+ */
+public class AsyncProducer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动Producer实例
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        for (int i = 0; i < 100; i++) {
+            final int index = i;
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            // SendCallback接收异步返回结果的回调
+            producer.send(msg, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("%-10d OK %s %n", index,
+                            sendResult.getMsgId());
+                }
+                @Override
+                public void onException(Throwable e) {
+                    System.out.printf("%-10d Exception %s %n", index, e);
+                    e.printStackTrace();
+                }
+            });
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+    }
+}
---
+++ src/main/java/com/learning/wheel/sort/SortTest.java
@@ -0,0 +1,110 @@
+package com.learning.wheel.sort;
+
+/**
+ * @program wheel
+ * @author: hangwei
+ * @create: 2019/08/28 14:42
+ */
+public class SortTest {
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j + 1] < array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] selectionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    /**
+     * 插入排序
+     *
+     * @param array
+     * @return
+     */
+    public static int[] insertionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        int current;
+        for (int i = 0; i < array.length - 1; i++) {
+            current = array[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < array[preIndex]) {
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
+            }
+            array[preIndex + 1] = current;
+        }
+        return array;
+    }
+
+    /**
+     * 快速排序方法
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] QuickSort(int[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition(array, start, end);
+        if (smallIndex > start)
+            QuickSort(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            QuickSort(array, smallIndex + 1, end);
+        return array;
+    }
+    /**
+     * 快速排序算法——partition
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+    /**
+     * 交换数组内两个元素
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
