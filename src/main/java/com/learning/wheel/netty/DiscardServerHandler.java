d15574b0cbc810f847acdf9ae35dc5594ebcd152
hangwei <hangwei@shinemo.com>
2020-09-09 17:18:27 +0800

    c

---
+++ src/main/java/com/learning/wheel/netty/DiscardServerHandler.java
@@ -0,0 +1,33 @@
+package com.learning.wheel.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @program wheel
+ * @author: hangwei
+ * @create: 2020/06/30 11:32
+ */
+
+/**
+ * 处理服务端的channel
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try{
+        //do something with msg
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
